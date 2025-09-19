5948bbc218e81276bb3da5a65f1ef0436185e28c
Lance Vincent Salera <salera.lancevincent@gmail.com>
2025-09-19 10:44:10 +0800

    fix: array indexing offset

--- src/boardarray.h
+++ src/boardarray.h
@@ -14,14 +14,36 @@ class BoardArray : public Board {
         }
 
         void add(Entry* entry) {
-            // TODO: IMPLEMENT THIS FUNCTION
-            // ALGORITHM IS PROVIDED IN INSTRUCTIONS.TXT
+            int pos = -1;
+
+            for(int i=0; i < index; i++) {
+                Entry existing = array[i];
+                if(entry->compare(&existing)) {
+                    pos = i;
+                    break;
+                }
+            }
+
+            if(pos == -1) {
+                if(index < SIZE) {
+                    array[index++] = *entry;
+                } else {
+                    cout << entry->name << "'s score is too low to be added!" << endl;
+                }
+                return;
+            }
+
+            for(int i=index; i > pos; i--) {
+                array[i] = array[i - 1];
+            }
+            array[pos] = *entry;
+            if(index < SIZE) index++;
         }
 
         void print() {
             for (int i = 0; i < index; i++) {
                 cout << i + 1 << ". ";
-                (array + i * sizeof(Entry))->print();
+                array[i].print();
             }
         }
 };
