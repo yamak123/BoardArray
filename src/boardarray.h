7b61750a3ecd6a81558d061cd96225501c73ebba
Johnmark C. Tanaleon <johnmarktanaleon1@gmail.com>
2025-09-19 13:29:05 +0800

    initial add function implementaions

--- src/boardarray.h
+++ src/boardarray.h
@@ -14,8 +14,9 @@ class BoardArray : public Board {
         }
 
         void add(Entry* entry) {
-            // TODO: IMPLEMENT THIS FUNCTION
-            // ALGORITHM IS PROVIDED IN INSTRUCTIONS.TXT
+            for(int i = 0; i < index;i++){
+                if(entry->compare(array[]))
+            }
             return;
         }
 
